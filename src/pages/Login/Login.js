b3cf5c7c9856420284305ec648fda7595f6f5851
gavand-sagar <gavand.sagar.s@gmail.com>
2022-10-19 12:46:08 +0530

    chagned login heading

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -31,7 +31,7 @@ export default function Login() {
     return (
         <div className='login-container'>
             <div className='login-form'>
-                <h1>Log in!</h1>
+                <h1>Tell us Who you are?</h1>
                 <div>
                     <input
                         value={username}
